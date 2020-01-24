a707256f0d307fd774005d6e632e7b57970bc854
Zakyyy <ahmed.abdelghafour@student.guc.edu.eg>
2020-01-24 20:21:10 +0200

    Sample app

---
+++ GbReactLibrary/android/src/main/java/com/gameball/RNGbReactLibraryModule.java
@@ -0,0 +1,22 @@
+
+package com.gameball;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.Callback;
+
+public class RNGbReactLibraryModule extends ReactContextBaseJavaModule {
+
+  private final ReactApplicationContext reactContext;
+
+  public RNGbReactLibraryModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+    this.reactContext = reactContext;
+  }
+
+  @Override
+  public String getName() {
+    return "RNGbReactLibrary";
+  }
+}
---
+++ ios/Pods/Headers/Public/react-native-static-server/GCDWebServerFileRequest.h
@@ -0,0 +1,1 @@
+../../../../../node_modules/react-native-static-server/ios/GCDWebServer/Requests/GCDWebServerFileRequest.h
